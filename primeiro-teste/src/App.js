19dc3b07bde70c9d3834ae0f391cb75a3e0b1245
PedroGimenes <pedro.gimenes@etec.sp.gov.br>
2025-08-08 17:16:13 -0300

    atualizando

--- primeiro-teste/src/App.js
+++ primeiro-teste/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import Frase from './components/Frase';
+import DigaMeuNome from './components/DigaMeuNome';
 
 function App() {
+ // prompt('Digite seu nome: ') 
+ // const nome = 'Pao' //variavel
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MEU APP REACT</h1>
+      <Frase />
+      <HelloWorld />
+      <Frase />
+      <DigaMeuNome nome={"Parker"} />
+      <p>Outro paragrafo</p>
+      
     </div>
   );
 }
